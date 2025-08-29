e912eac0f8062592f3301c338c8b9c35420adc01
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-08-29 07:41:44 +0300

    Update Booking  migratiion

---
+++ LagonaDahab.Infrastructure/Migrations/20250826134724_UpdationBooking.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LagonaDahab.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdationBooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "TotalPrice",
+                table: "Bookings",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "CheckOutDate",
+                table: "Bookings",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "CheckInDate",
+                table: "Bookings",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalPrice",
+                table: "Bookings",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CheckOutDate",
+                table: "Bookings",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CheckInDate",
+                table: "Bookings",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
