0cbaf099dec7dbda3a89fed955f80fcfecf44027
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-09-08 06:36:48 +0300

    Create VillaRoomsAvailable_Count

--- LagonaDahab.Application/Common/Utility/SD.cs
+++ LagonaDahab.Application/Common/Utility/SD.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LagonaDahab.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,15 +12,43 @@ namespace LagonaDahab.Application.Common.Utility
     {
         // / Constants for roles
 
-        public const string Role_Admin    = "Admin";
+        public const string Role_Admin = "Admin";
         public const string Role_Customer = "Customer";
 
-        public const string StatusPending   = "Pending";
-        public const string StatusApproved  = "Approved";
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
         public const string StatusCheckedIn = "ChechedIn";
         public const string StatusCompleted = "Completed";
         public const string StatusCancelled = "Cancelled";
-        public const string StatusRefunded  = "Refunded";
+        public const string StatusRefunded = "Refunded";
+
+
+        public static int VillaRoomsAvailable_Count(
+            int villaId,
+            List<VillaNumber> villaNumberList,
+            DateOnly checkInDate,
+            int nights,
+            List<Booking> bookings)
+        {
+            // Get the total number of rooms in the villa
+            var roomsInVilla = villaNumberList.Count(x => x.VillaId == villaId);
+
+            // For each night, calculate the number of booked rooms and determine availability
+            return Enumerable.Range(0, nights)
+                .Select(i =>
+                {
+                    var bookingsForDate = bookings
+                        .Where(b => b.villaId == villaId &&
+                                   b.CheckInDate <= checkInDate.AddDays(i) &&
+                                   b.CheckOutDate > checkInDate.AddDays(i))
+                        .Select(b => b.Id)
+                        .Distinct()
+                        .Count();
+                    // Available rooms for this specific date
+                    return roomsInVilla - bookingsForDate;
+                })
+                .Min();
+        }
 
     }
 }
